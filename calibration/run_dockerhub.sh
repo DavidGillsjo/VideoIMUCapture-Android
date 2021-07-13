bbc6af51eaf8ccb54c22998508fc36a46a53563b
David Gillsjö <david.gillsjo@math.lth.se>
2021-07-13 11:58:31 +0200

    Pull docker image before running

--- calibration/run_dockerhub.sh
+++ calibration/run_dockerhub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Usage: [ENV_OPTS] ./run_local [CMD] [ARGS]
 ./hooks/pre_run
-USE_NVIDIA=0 IMAGE=${IMAGE-"davidgillsjo/videoimucapture-calibration"} ./../libs/dockers/common/run.sh "$@"
+PULL=1 USE_NVIDIA=0 IMAGE=${IMAGE-"davidgillsjo/videoimucapture-calibration"} ./../libs/dockers/common/run.sh "$@"
