e1a6c586f39818afe2dc16c3744167c931de2480
Johnny <hcchengaa@ust.hk>
2023-02-01 15:23:22 +0800

    support for uncalibrated magnetometer data

--- android_app/app/src/main/java/se/lth/math/videoimucapture/IMUManager.java
+++ android_app/app/src/main/java/se/lth/math/videoimucapture/IMUManager.java
@@ -17,11 +17,12 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Iterator;
 
+
 public class IMUManager extends SensorEventCallback {
     private static final String TAG = "IMUManager";
     private int ACC_TYPE;
     private int GYRO_TYPE;
-
+    private int MAG_TYPE;
 
     // if the accelerometer data has a timestamp within the
     // [t-x, t+x] of the gyro data at t, then the original acceleration data
@@ -46,11 +47,13 @@ public class IMUManager extends SensorEventCallback {
         long timestamp;
         float[] acc_values;
         float[] gyro_values;
+        float[] mag_values;
 
-        SyncedSensorPacket(long time, float[] acc, float[] gyro) {
+        SyncedSensorPacket(long time, float[] acc, float[] gyro, float[] mag) {
             timestamp = time;
             acc_values = acc;
             gyro_values = gyro;
+            mag_values = mag;
         }
     }
 
@@ -58,9 +61,11 @@ public class IMUManager extends SensorEventCallback {
     private SensorManager mSensorManager;
     private Sensor mAccel;
     private Sensor mGyro;
+    private Sensor mMag;
 
     private int linear_acc; // accuracy
     private int angular_acc;
+    private int mag_acc;
 
     private volatile boolean mRecordingInertialData = false;
     private RecordingWriter mRecordingWriter = null;
@@ -68,6 +73,7 @@ public class IMUManager extends SensorEventCallback {
 
     private Deque<SensorPacket> mGyroData = new ArrayDeque<>();
     private Deque<SensorPacket> mAccelData = new ArrayDeque<>();
+    private Deque<SensorPacket> mMagData = new ArrayDeque<>();
 
     public IMUManager(Activity activity) {
         super();
@@ -75,7 +81,7 @@ public class IMUManager extends SensorEventCallback {
         setSensorType();
         mAccel = mSensorManager.getDefaultSensor(ACC_TYPE);
         mGyro = mSensorManager.getDefaultSensor(GYRO_TYPE);
-
+        mMag = mSensorManager.getDefaultSensor(MAG_TYPE);
     }
 
     private void setSensorType() {
@@ -84,10 +90,59 @@ public class IMUManager extends SensorEventCallback {
         else
             ACC_TYPE = Sensor.TYPE_ACCELEROMETER;
         GYRO_TYPE = Sensor.TYPE_GYROSCOPE_UNCALIBRATED;
+        MAG_TYPE = Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED;
+    }
+
+    private float[] linearInterpolate(Deque<SensorPacket> queue, SensorPacket reference) {
+        // target's timestamp is assumed to be within the range of queue's timestamp
+
+        SensorPacket left = null;
+        SensorPacket right = null;
+        Iterator<SensorPacket> itr = queue.iterator();
+
+        // find the closest data right next to gyro's timestamp
+        while (itr.hasNext()) {
+            SensorPacket packet = itr.next();
+
+            // using <= and >= as sometimes there is not enough data & left/right is null
+            if (packet.timestamp <= reference.timestamp) {
+                left = packet;
+            } else if (packet.timestamp >= reference.timestamp) {
+                right = packet;
+                break;
+            }
+        }
+
+        float[] data;
+        if (reference.timestamp - left.timestamp <= mInterpolationTimeResolution) {
+            data = left.values;
+        } else if (right.timestamp - reference.timestamp <= mInterpolationTimeResolution) {
+            data = right.values;
+        } else {
+            float ratio = (float)(reference.timestamp - left.timestamp) /
+                    (right.timestamp - left.timestamp);
+            data = new float[left.values.length]; // could vary depending on sensor type
+            for (int i = 0 ; i < left.values.length ; i++) {
+                data[i] = left.values[i] +
+                        (right.values[i] - left.values[i]) * ratio;
+            }
+        }
+
+        // Remove the current element from the iterator and the list.
+        for (Iterator<SensorPacket> iterator = mAccelData.iterator(); iterator.hasNext(); ) {
+            SensorPacket packet = iterator.next();
+            if (packet.timestamp < left.timestamp) {
+                iterator.remove();
+            } else {
+                break;
+            }
+        }
+
+        return data;
     }
 
     public Boolean sensorsExist() {
-        return (mAccel != null) && (mGyro != null);
+        return (mAccel != null) && (mGyro != null) && (mMag != null);
     }
 
     public void startRecording(RecordingWriter recordingWriter) {
@@ -106,6 +161,8 @@ public class IMUManager extends SensorEventCallback {
             linear_acc = accuracy;
         } else if (sensor.getType() == GYRO_TYPE) {
             angular_acc = accuracy;
+        } else if (sensor.getType() == MAG_TYPE) {
+            mag_acc = accuracy;
         }
     }
 
@@ -113,61 +170,36 @@ public class IMUManager extends SensorEventCallback {
     // Because the sensor events are delivered to the handler thread in order,
     // no need for synchronization here
     private SyncedSensorPacket syncInertialData() {
-        if (mGyroData.size() >= 1 && mAccelData.size() >= 2) {
+        if (mGyroData.size() >= 1 && mAccelData.size() >= 2 && mMagData.size() >= 2) {
+            // take gyro as reference
             SensorPacket oldestGyro = mGyroData.peekFirst();
+
+            // interpolate accel and mag
             SensorPacket oldestAccel = mAccelData.peekFirst();
             SensorPacket latestAccel = mAccelData.peekLast();
-            if (oldestGyro.timestamp < oldestAccel.timestamp) {
+            SensorPacket oldestMag = mMagData.peekFirst();
+            SensorPacket latestMag = mMagData.peekLast();
+
+            if (oldestGyro.timestamp < oldestAccel.timestamp || oldestGyro.timestamp < oldestMag.timestamp) {
+                // check if gyro data is within range of mag & accel data
                 Log.w(TAG, "throwing one gyro data");
                 mGyroData.removeFirst();
             } else if (oldestGyro.timestamp > latestAccel.timestamp) {
                 Log.w(TAG, "throwing #accel data " + (mAccelData.size() - 1));
                 mAccelData.clear();
                 mAccelData.add(latestAccel);
-            } else { // linearly interpolate the accel data at the gyro timestamp
-                SensorPacket leftAccel = null;
-                SensorPacket rightAccel = null;
-                Iterator<SensorPacket> itr = mAccelData.iterator();
-                while (itr.hasNext()) {
-                    SensorPacket packet = itr.next();
-                    if (packet.timestamp <= oldestGyro.timestamp) {
-                        leftAccel = packet;
-                    } else if (packet.timestamp >= oldestGyro.timestamp) {
-                        rightAccel = packet;
-                        break;
-                    }
-                }
+            } else if (oldestGyro.timestamp > latestMag.timestamp) {
+                Log.d(TAG, "throwing #mag data " + (mMagData.size() - 1));
+                mMagData.clear();
+                mMagData.add(latestMag);
+            } else { // linearly interpolate the accel & mag data at the gyro timestamp
+                float[] acc_data = linearInterpolate(mAccelData, oldestGyro);
+                float[] mag_data = linearInterpolate(mMagData, oldestGyro);
 
-                float[] acc_data;
-                if (oldestGyro.timestamp - leftAccel.timestamp <=
-                        mInterpolationTimeResolution) {
-                    acc_data = leftAccel.values;
-                } else if (rightAccel.timestamp - oldestGyro.timestamp <=
-                        mInterpolationTimeResolution) {
-                    acc_data = rightAccel.values;
-                } else {
-                    float ratio = (float)(oldestGyro.timestamp - leftAccel.timestamp) /
-                            (rightAccel.timestamp - leftAccel.timestamp);
-                    acc_data = new float[leftAccel.values.length];
-                    for (int i = 0 ; i<leftAccel.values.length ; i++) {
-                        acc_data[i] = leftAccel.values[i] +
-                                (rightAccel.values[i] - leftAccel.values[i]) * ratio;
-                    }
-                }
+                mGyroData.removeFirst(); // remove the processed data
 
-                mGyroData.removeFirst();
-                for (Iterator<SensorPacket> iterator = mAccelData.iterator();
-                     iterator.hasNext(); ) {
-                    SensorPacket packet = iterator.next();
-                    if (packet.timestamp < leftAccel.timestamp) {
-                        // Remove the current element from the iterator and the list.
-                        iterator.remove();
-                    } else {
-                        break;
-                    }
-                }
                 return new SyncedSensorPacket(oldestGyro.timestamp,
-                        acc_data, oldestGyro.values);
+                        acc_data, oldestGyro.values, mag_data);
             }
         }
         return null;
@@ -176,13 +208,15 @@ public class IMUManager extends SensorEventCallback {
     private void writeData(SyncedSensorPacket packet) {
         RecordingProtos.IMUData.Builder imuBuilder =
                 RecordingProtos.IMUData.newBuilder()
-                .setTimeNs(packet.timestamp)
-                .setAccelAccuracyValue(linear_acc)
-                .setGyroAccuracyValue(angular_acc);
+                        .setTimeNs(packet.timestamp)
+                        .setAccelAccuracyValue(linear_acc)
+                        .setGyroAccuracyValue(angular_acc)
+                        .setMagAccuracyValue(mag_acc);
 
         for (int i = 0 ; i < 3 ; i++) {
             imuBuilder.addGyro(packet.gyro_values[i]);
             imuBuilder.addAccel(packet.acc_values[i]);
+            imuBuilder.addMag(packet.mag_values[i]);
         }
         if (ACC_TYPE == Sensor.TYPE_ACCELEROMETER_UNCALIBRATED) {
             for (int i = 3 ; i < 6 ; i++) {
@@ -194,27 +228,33 @@ public class IMUManager extends SensorEventCallback {
                 imuBuilder.addGyroDrift(packet.gyro_values[i]);
             }
         }
+        if (MAG_TYPE == Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED) {
+            for (int i = 3 ; i < 6 ; i++) {
+                imuBuilder.addMagBias(packet.mag_values[i]);
+            }
+        }
+
         mRecordingWriter.queueData(imuBuilder.build());
     }
 
     private void writeMetaData() {
         RecordingProtos.IMUInfo.Builder builder = RecordingProtos.IMUInfo.newBuilder();
         if (mGyro != null) {
-            builder.setGyroInfo(mGyro.toString())
-                    .setGyroResolution(mGyro.getResolution());
+            builder.setGyroInfo(mGyro.toString()).setGyroResolution(mGyro.getResolution());
         }
         if (mAccel != null) {
-            builder.setAccelInfo(mAccel.toString())
-                    .setAccelResolution(mAccel.getResolution());
+            builder.setAccelInfo(mAccel.toString()).setAccelResolution(mAccel.getResolution());
+        }
+        if (mMag != null) {
+            builder.setMagInfo(mMag.toString()).setMagResolution(mMag.getResolution());
         }
         builder.setSampleFrequency(getSensorFrequency());
 
         //Store translation for sensor placement in device coordinate system.
         if (mSensorPlacement != null) {
-           builder.addPlacement(mSensorPlacement[3])
-                  .addPlacement(mSensorPlacement[7])
-                  .addPlacement(mSensorPlacement[11]);
-
+            builder.addPlacement(mSensorPlacement[3])
+                    .addPlacement(mSensorPlacement[7])
+                    .addPlacement(mSensorPlacement[11]);
         }
         mRecordingWriter.queueData(builder.build());
     }
@@ -234,14 +274,21 @@ public class IMUManager extends SensorEventCallback {
         if (event.sensor.getType() == ACC_TYPE) {
             SensorPacket sp = new SensorPacket(event.timestamp, event.values);
             mAccelData.add(sp);
+
             updateSensorRate(event);
         } else if (event.sensor.getType() == GYRO_TYPE) {
             SensorPacket sp = new SensorPacket(event.timestamp, event.values);
             mGyroData.add(sp);
-            SyncedSensorPacket syncedData = syncInertialData();
-            if (syncedData != null && mRecordingInertialData) {
-                writeData(syncedData);
+
+            // sync data
+            if (mRecordingInertialData) {
+                SyncedSensorPacket syncedData = syncInertialData();
+                if (syncedData != null)
+                    writeData(syncedData);
             }
+        } else if (event.sensor.getType() == MAG_TYPE) {
+            SensorPacket sp = new SensorPacket(event.timestamp, event.values);
+            mMagData.add(sp);
         }
     }
 
@@ -268,10 +315,9 @@ public class IMUManager extends SensorEventCallback {
         mSensorThread.start();
         // Blocks until looper is prepared, which is fairly quick
         Handler sensorHandler = new Handler(mSensorThread.getLooper());
-        mSensorManager.registerListener(
-                this, mAccel, mSensorRate, sensorHandler);
-        mSensorManager.registerListener(
-                this, mGyro, mSensorRate, sensorHandler);
+        mSensorManager.registerListener(this, mAccel, mSensorRate, sensorHandler);
+        mSensorManager.registerListener(this, mGyro, mSensorRate, sensorHandler);
+        mSensorManager.registerListener(this, mMag, mSensorRate, sensorHandler);
     }
 
     /**
@@ -283,6 +329,7 @@ public class IMUManager extends SensorEventCallback {
         }
         mSensorManager.unregisterListener(this, mAccel);
         mSensorManager.unregisterListener(this, mGyro);
+        mSensorManager.unregisterListener(this, mMag);
         mSensorManager.unregisterListener(this);
         mSensorThread.quitSafely();
         stopRecording();
